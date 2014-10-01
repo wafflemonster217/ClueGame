cd8831596b9716e244136c1da7bb03134ee6f319
Nate Keller <nkeller@mines.edu>
2014-10-01 17:32:36 -0600

    Work on passing tests

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -7,107 +7,53 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class ClueGame {
-	Map<Character,String> rooms = new HashMap<Character,String>();
+	private Map<Character,String> rooms;
 	private Board theBoard;
 	private String layoutFile;
 	private String configFile;
 	
 	public ClueGame(String layout,String legend)
 	{
+		rooms = new HashMap<Character,String>()
 		layoutFile = layout;
 		configFile = legend;
-		theBoard = new Board();
+		theBoard = new Board(layout);
 	}
 	
-	public void loadRoomConfig()
-		{
-			FileReader fr = null;
-			Scanner sc = null;
-			String line = " ";
-			String[] splitLine;
-			Character tempKey = ' ';
-			String tempValue = " ";
-			
-			//System.out.println("room config");
-			try
-			{
-				fr = new FileReader(configFile);
-				
-			}
-			catch(FileNotFoundException e)
-			{
-				System.out.println(e);
-			}
-			
-			sc = new Scanner(fr);
-			
-			while(sc.hasNextLine())
-			{
-				
-				line = sc.nextLine();
-				splitLine = line.split(",");
-					
-				tempKey =  line.charAt(0);
-				tempValue = splitLine[1];
+	public void loadRoomConfig() throws FileNotFoundException, BadConfigFormatException {
+		String line;
+		String[] splitLine;
+		Character tempKey;
+		String tempValue;
 
-				
-				//tempValue = tempValue.toUpperCase();
-				tempValue = tempValue.trim();
-				
-				rooms.put(tempKey,tempValue);
-									
-			}
-						
-			sc.close();
-			
-		//System.out.println(rooms);
+		Scanner scan = new Scanner(new FileReader(configFile));
 
+		while(scan.hasNextLine()) {
+			line = scan.nextLine();
+			splitLine = line.split(",");
+
+			tempKey =  line.charAt(0);
+			tempValue = splitLine[1];
+
+			tempValue = tempValue.trim();
+
+			rooms.put(tempKey,tempValue);				
 		}
-	
-	public void loadConfigFiles(){
 
-		//loadRoomConfig();
+		scan.close();
+	}
+
+	
+	
+	public void loadConfigFiles() throws FileNotFoundException, BadConfigFormatException{
 		loadRoomConfig();
-		theBoard.rooms = rooms;
-	//	System.out.println("room config loaded : " + rooms);
+		theBoard.setRooms(rooms);
+
 		
 		theBoard.loadBoardConfig();
 	
 	}
-	
-	// need to get rid of main
-	public static void main(String[] args)
-	{
-		
-		ClueGame game = new ClueGame("ClueLayout.csv","ClueLegend.txt");
-		
-		game.loadConfigFiles();
-		
-		
-		System.out.println(game.getBoard().getNumRows());
-		System.out.println(game.getBoard().getCellAt(5,5));
-		System.out.println(game.getBoard().getRooms());
-		System.out.println(game.getBoard().getCellAt(0, 0).isRoom());
-		
-		
-		/*
-		for(int r = 0; r < game.getBoard().getNumRows(); r++)
-		{
-			
-			for(int c = 0; c < game.getBoard().getNumColumns(); c++)
-			{
-				
-				System.out.println(game.getBoard().getCellAt(r,c) +  " is it a doorway? " + game.getBoard().getCellAt(r,c).isDoorway());
-				
-				
-				
-			}
-			
-			
-			
-		}
-		*/
-	}
+
 
 	public Board getBoard() {
 		return theBoard;
